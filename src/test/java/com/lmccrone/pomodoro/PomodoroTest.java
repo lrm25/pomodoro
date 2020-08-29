3365e916fbc5331df88077c8988dbd1b59637546
Luke McCrone <luke.r.mccrone@gmail.com>
2020-08-28 20:02:12 -0600

    using maven

---
+++ src/test/java/com/lmccrone/pomodoro/PomodoroTest.java
@@ -0,0 +1,12 @@
+package com.lmccrone.pomodoro_test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PomodoroTest {
+    @Test
+    public void dummyTest() {
+        assertEquals("this will fail", "test", "test2");
+    }
+}
